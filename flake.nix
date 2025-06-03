3b1c11c6ebb401e3a6953e440d9efa939f153ead
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-06-03 15:44:10 -0400

    downgrade nixpkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "bitwarden rofi client";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs?ref=nixos-unstable;
+    nixpkgs.url = github:nixos/nixpkgs?ref=nixos-24.11;
   };
 
   outputs = {
